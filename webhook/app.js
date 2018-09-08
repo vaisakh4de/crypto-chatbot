40b5f31a965ce519858ce7db0b7341fe1ce94138
vaisakh4de <43033562+vaisakh4de@users.noreply.github.com>
2018-09-08 19:35:34 +0530

    updated app.js

--- webhook/app.js
+++ webhook/app.js
@@ -18,30 +18,37 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.post('/get-movie-details', (req, res) => {
+app.post('/webhook', (req, res) => {
 
+    const actionNeeded = req.body.queryResult.action;
     const symbol = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.fsym;
     const reqUrl = encodeURI(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD`);
-    https.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
-        responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
-        responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = `value of ${symbol} is ${movie.USD} USD`;
-
+    
+    if (actionNeeded == 'app')
+    {
+        https.get(reqUrl, (responseFromAPI) => {
+            let completeResponse = '';
+            responseFromAPI.on('data', (chunk) => {
+                completeResponse += chunk;
+            });
+            responseFromAPI.on('end', () => {
+                const movie = JSON.parse(completeResponse);
+                let dataToSend = `value of ${symbol} is ${movie.USD} USD`;
+    
+                return res.json({
+                    fulfillmentText: dataToSend,
+                    source: 'webhook'
+                });
+            });
+        }, (error) => {
             return res.json({
                 fulfillmentText: dataToSend,
-                source: 'get-movie-details'
+                source: 'webhook'
             });
-        });
-    }, (error) => {
-        return res.json({
-            fulfillmentText: dataToSend,
-            source: 'get-movie-details'
-        });
-    });
+        });   
+    }
+
 });
+
 
 module.exports = app;
