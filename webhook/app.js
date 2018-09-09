92a4d34697236bdc4a26ed066a26a63f83bf470e
vaisakh4de <43033562+vaisakh4de@users.noreply.github.com>
2018-09-09 12:58:55 +0530

    now add country code as option

--- webhook/app.js
+++ webhook/app.js
@@ -21,11 +21,12 @@ app.use((req, res, next) => {
 app.post('/webhook', (req, res) => {
 
     const actionNeeded = req.body.queryResult.action;
-    const symbol = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.fsym;
-    const reqUrl = encodeURI(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD`);
+    const symbol =  req.body.queryResult.parameters.fsym;
+    const country = req.body.queryResult.parameters.tsyms ? req.body.queryResult.parameters.tsyms : 'USD';
     
-    if (actionNeeded == 'app')
+    if (actionNeeded === 'price')
     {
+        const reqUrl = encodeURI(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=${country}`);
         https.get(reqUrl, (responseFromAPI) => {
             let completeResponse = '';
             responseFromAPI.on('data', (chunk) => {
@@ -33,8 +34,9 @@ app.post('/webhook', (req, res) => {
             });
             responseFromAPI.on('end', () => {
                 const movie = JSON.parse(completeResponse);
-                let dataToSend = `value of ${symbol} is ${movie.USD} USD`;
-    
+                
+                let dataToSend = `value of ${symbol} in ${country} is ${movie.USD} `;
+                console.log(dataToSend);
                 return res.json({
                     fulfillmentText: dataToSend,
                     source: 'webhook'
